15af9991a8840096ea10d0018f0c8e060c620247
Nick Bottomley <nhbottomley@gmail.com>
2014-09-11 15:45:54 -0700

    Build out board class

---
+++ app/app.js
@@ -0,0 +1,4 @@
+module.exports = {
+  board: require( "./board" ),
+  solution: require( "./solution" )
+};
--- app/board.js
+++ app/board.js
@@ -1,29 +1,98 @@
 var _ = require( "lodash" );
 
-var BOARD_WIDTH = 9;
-var BOARD_HEIGHT = 9;
+var BOARD_SIZE = 9;
 
-var SUB_BOARD_WIDTH = 3;
-var SUB_BOARD_HEIGHT = 3;
+var SUB_BOARD_SIZE = 3;
 
-function SubBoard () {
-  var board = [];
-  for ( var i = 0; i < SUB_BOARD_HEIGHT; i++ ) {
-    board.push( [] );
-    for ( var j = 0; j < SUB_BOARD_WIDTH; j++ ) {
-      board[i].push( "" );
+function Board ( board ) {
+  if ( board == null ) {
+    board = [];
+    for ( var i = 0; i < BOARD_SIZE; i++ ) {
+      board.push( [] );
+      for ( var j = 0; j < BOARD_SIZE; j++ ) {
+        board[i].push( "-" );
+      }
     }
   }
   this._board = board;
 }
 
-SubBoard.prototype.valid = function () {
-  var values = _.flatten( this._board ).map( Number );
-  if (
-    values.some( isNaN ) ||
-    values.length !== _.unique( values ).length
-  ) {
+Board.prototype.get = function ( x, y ) {
+  if ( this._board[y] ) {
+    return this._board[y][x];
+  }
+  return null;
+};
+
+Board.prototype.set = function ( x, y, value ) {
+  if ( y >= this._board.length || x >= this._board[0].length ) {
+    throw new Error( "Attempting to set cell out of range" );
+  }
+  this._board[y][x] = value;
+  return this;
+};
+
+Board.prototype.columnValues = function ( i ) {
+  return this._board.map( function ( row ) {
+    return row[i];
+  });
+};
+
+Board.prototype.rowValues = function ( i ) {
+  return this._board[i].slice();
+};
+
+Board.prototype.subBoardValues = function ( x, y ) {
+  if ( x > BOARD_SIZE / SUB_BOARD_SIZE - 1 || y > BOARD_SIZE / SUB_BOARD_SIZE - 1 ) {
+    throw new Error( "Attempting to read sub-board out of range" );
+  }
+  var values = [];
+  var startX = x * SUB_BOARD_SIZE;
+  var endX = startX + SUB_BOARD_SIZE;
+  var startY = y * SUB_BOARD_SIZE;
+  var endY = startY + SUB_BOARD_SIZE;
+
+  for ( var i = startX; i < endX; i++ ) {
+    for ( var j = startY; j < endY; j++ ) {
+      values.push( this.get( i, j ) );
+    }
+  }
+  return values;
+};
+
+Board.prototype.eachRow = function ( fn ) {
+  for ( var i = 0; i < BOARD_SIZE; i++ ) {
+    fn( this.rowValues( i ) );
+  }
+};
+
+Board.prototype.eachColumn = function ( fn ) {
+  for ( var i = 0; i < BOARD_SIZE; i++ ) {
+    fn( this.columnValues( i ) );
+  }
+};
+
+Board.prototype.eachSubBoard = function ( fn ) {
+  var max = BOARD_SIZE / SUB_BOARD_SIZE - 1;
+  for ( var i = 0; i < max; i++ ) {
+    for ( var j = 0; j < max; j++ ) {
+      fn( this.subBoardValues( i, j ) );
+    }
+  }
+};
+
+Board.numberSetIsValid = function ( arr ) {
+  if ( arr.length !== BOARD_SIZE ) {
     return false;
   }
-  return true;
+  return arr
+    .map( Number )
+    .sort( function ( a, b ) {
+      return a - b;
+    })
+    .every( function ( value, i ) {
+      return value === i + 1;
+    });
 };
+
+module.exports = Board;
