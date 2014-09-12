b1698a134daea3b65f15c8f439c34d39b36be113
Nick Bottomley <nhbottomley@gmail.com>
2014-09-12 13:23:38 -0700

    modifying public interface

--- app/app.js
+++ app/app.js
@@ -10,10 +10,8 @@ var BoardController = require( "./board-controller" );
 var solution = fixtures.solution;
 var problem = fixtures.problem;
 
-var sudoku;
-
-
-sudoku = ( function () {
+var sudoku = ( function () {
+  var body = $( "body" );
   var main = $( ".main" );
   var won = $( ".won" );
 
@@ -22,38 +20,60 @@ sudoku = ( function () {
     won.slideUp();
   });
 
-    return {
-    // _game is exposed for debugging and inspection; doesn't need to be
-    _game: {},
-    makeBoard: function () {
-      // generator creates values 0-8 rather than 1-9
-      var generated = _sudoku.makepuzzle().map( function ( item ) {
-        if ( typeof item === "number" ) {
-          item += 1;
-        }
-        return item;
-      });
-      var board = [];
-      while ( generated.length ) {
-        board.push( generated.splice( 0, 9 ) );
+  function toggleCells ( onOff ) {
+    main.find( ".board .cell--input" ).prop( "disabled", onOff );
+  }
+
+  function makeBoard () {
+    // generator creates values 0-8 rather than 1-9
+    var generated = _sudoku.makepuzzle().map( function ( item ) {
+      if ( typeof item === "number" ) {
+        item += 1;
       }
-      return board;
-    },
+      return item;
+    });
+    var board = [];
+    while ( generated.length ) {
+      board.push( generated.splice( 0, 9 ) );
+    }
+    return board;
+  }
+
+  function onWin () {
+    toggleCells( true );
+    won.slideDown();
+  }
+
+  return {
+    _game: {},
     newGame: function ( b ) {
+      toggleCells( false );
       main.empty();
-      var board = new Board( b || this.makeBoard() );
+      var board = new Board( b || makeBoard() );
       var view = new BoardView();
       var ctrl = new BoardController( board, view );
       main.append( view.element );
+      // _game is exposed for debugging and inspection; doesn't need to be
       this._game = {
         board: board,
         view: view,
         ctrl: ctrl
       };
-      board.on( "complete", this.won.bind( this ) );
+      board.on( "complete", onWin );
+      board.emit( "set" );
     },
-    won: function () {
-      won.slideDown();
+    newSolvedGame: function () {
+      this.newGame([
+        [5,3,4,6,7,8,9,1,2],
+        [6,7,2,1,9,5,3,4,8],
+        [1,9,8,3,4,2,5,6,7],
+        [8,5,9,7,6,1,4,2,3],
+        [4,2,6,8,5,3,7,9,1],
+        [7,1,3,9,2,4,8,5,6],
+        [9,6,1,5,3,7,2,8,4],
+        [2,8,7,4,1,9,6,3,5],
+        [3,4,5,2,8,6,1,7,9]
+      ]);
     }
   };
 })();
